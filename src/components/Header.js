d0dd4193b83a321b046e94937076d3ec810c2930
Christina Persson <christina@MacBook-Pro.local>
2020-04-22 15:33:05 +0200

    Testing another solution for text animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Typing from "react-typing-animation";
 
 import { DownArrow } from "./DownArrow";
 
@@ -6,9 +7,10 @@ export const Header = () => {
   return (
     <header className="header">
       <section className="header-section">
-        <h1>
-          <span className="header-span"></span>
-        </h1>
+        <Typing loop="true" hideCursor="true" speed={150}>
+          <span className="header-span">Web Developer</span>
+          <Typing.Reset count={1} delay={450} />
+        </Typing>
       </section>
       <a href="#nav" className="header-button">
         <DownArrow />
