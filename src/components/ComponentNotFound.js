134b7f3714c1610b3cfaf6ba45f70fd4a9af354b
Christina Persson <christina@MacBook-Pro.local>
2020-04-26 16:18:18 +0200

    added 404 page

--- src/components/ComponentNotFound.js
+++ src/components/ComponentNotFound.js
@@ -4,7 +4,7 @@ import animation from "animations/404-space-error.json";
 
 export const ComponentNotFound = () => {
   return (
-    <section id="404" className="main-wrapper">
+    <section id="404">
       <div className="testing"></div>
       <h3>OH NO 🤭</h3>
       <Lottie
